f17849d18642ef687035b7d7f191ec08e636ac21
carlosltn0203 <145719493+carlosltn0203@users.noreply.github.com>
2024-05-30 16:12:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var fechaSeleccionada = $("#fecha").val();
 
         $.ajax({
-            url: "https://github.com/carlosltn0203/json-/blob/main/api/dummy.js",
+            url: "https://github.com/carlosltn0203/json-/blob/1698d1d430ec31f1a884a5c29b6cbd8d99bdbade/api/dummy.js",
             method: "GET",
             data: { fecha: fechaSeleccionada },
             success: function(data) {
